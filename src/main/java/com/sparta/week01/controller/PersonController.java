e4c3f44e3a2005a207929dfe43c8c401719a0fcd
성나영 <na3150@gmail.com>
2022-01-06 22:03:14 +0900

    1주차 완료 + 숙제

---
+++ src/main/java/com/sparta/week01/controller/PersonController.java
@@ -0,0 +1,20 @@
+package com.sparta.week01.controller;
+
+import com.sparta.week01.Course;
+import com.sparta.week01.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @GetMapping("/myinfo")
+    public Person getCourses() {
+        Person person = new Person();
+        person.setName("성나영");
+        person.setAge(23);
+        person.setJob("student");
+        return person;
+    }
+
+}
